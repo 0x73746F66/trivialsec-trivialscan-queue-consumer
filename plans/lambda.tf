ce9a0a781c83aa16e0b081ff7f7566b3c6e2b3ac
chris <chris@trivialsec.com>
2023-02-14 11:50:38 +1100

    feat: manage log group for lambda

--- plans/lambda.tf
+++ plans/lambda.tf
@@ -40,3 +40,9 @@ resource "aws_lambda_event_source_mapping" "trivialscan_queue_consumer_source" {
   function_name    = aws_lambda_function.trivialscan_queue_consumer.arn
   batch_size       = local.queue_batch_size
 }
+
+resource "aws_cloudwatch_log_group" "trivialscan_logs" {
+  skip_destroy      = var.app_env == "Prod"
+  name              = "/aws/lambda/${aws_lambda_function.trivialscan_queue_consumer.function_name}"
+  retention_in_days = local.retention_in_days
+}
