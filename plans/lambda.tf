63c9756ee279c4804b2b80dc3de4d451b6e456b2
chris <chris@trivialsec.com>
2023-03-30 10:25:54 +1100

    fix: lambda tf

--- plans/lambda.tf
+++ plans/lambda.tf
@@ -10,18 +10,24 @@ resource "aws_lambda_function" "trivialscan_queue_consumer" {
   layers        = var.app_env == "Prod" ? ["arn:aws:lambda:ap-southeast-2:725887861453:layer:Dynatrace_OneAgent_1_261_5_20230309-143152_python:1"] : []
 
   environment {
-    variables = {
+    variables = var.app_env == "Prod" ? {
       APP_ENV = var.app_env
       APP_NAME = var.app_name
       LOG_LEVEL = var.log_level
-      STORE_BUCKET = "${data.terraform_remote_state.trivialscan_s3.outputs.trivialscan_store_bucket[0]}"
+      STORE_BUCKET = data.terraform_remote_state.trivialscan_s3.outputs.trivialscan_store_bucket[0]
       BUILD_ENV = var.build_env
       AWS_LAMBDA_EXEC_WRAPPER = "/opt/dynatrace" # Use the wrapper from the layer
-      DT_TENANT = var.app_env == "Prod" ? "xuf85063" : ""
-      DT_CLUSTER_ID = var.app_env == "Prod" ? "-1273248646" : ""
+      DT_TENANT = "xuf85063"
+      DT_CLUSTER_ID = "-1273248646"
       DT_CONNECTION_BASE_URL = "https://xuf85063.live.dynatrace.com"
-      DT_CONNECTION_AUTH_TOKEN = var.app_env == "Prod" ? var.dynatrace_token : ""
+      DT_CONNECTION_AUTH_TOKEN = var.dynatrace_token
       DT_OPEN_TELEMETRY_ENABLE_INTEGRATION = "true"
+    } : {
+      APP_ENV = var.app_env
+      APP_NAME = var.app_name
+      LOG_LEVEL = var.log_level
+      STORE_BUCKET = data.terraform_remote_state.trivialscan_s3.outputs.trivialscan_store_bucket[0]
+      BUILD_ENV = var.build_env
     }
   }
   lifecycle {
